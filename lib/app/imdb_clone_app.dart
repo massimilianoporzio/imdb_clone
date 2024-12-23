2b5e1a5236edd433316456ea614376cf351cbfb2
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2024-12-23 17:31:50 +0100

    slang setup (funz?)

--- lib/app/imdb_clone_app.dart
+++ lib/app/imdb_clone_app.dart
@@ -1,1 +1,10 @@
+import 'package:flutter/material.dart';
 
+class IMDBCloneApp extends StatelessWidget {
+  const IMDBCloneApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router();
+  }
+}
